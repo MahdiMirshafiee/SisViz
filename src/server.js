f60dd2417f838185e7243aab49938e68caa8a167
MahdiMirshafiee <Mirshafieemahdi001@gmail.com>
2025-07-21 13:07:08 +0330

    fetch data and show in server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
-const {getSystemSnapshot} = require("./services/sysinfo.js")
+const { getSystemSnapshot } = require("./services/sysinfo.js");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set("views", path.join(__dirname, "templates"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { message: "sisviz!" });
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", async (req, res) => {
+  const snapshot = await getSystemSnapshot();
+  res.render("index", { snapshot });
 });
 
 app.listen(PORT, () => {
